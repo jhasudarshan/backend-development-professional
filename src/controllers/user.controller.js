dcd9776ea51a105e47587c0a0b6c69f4fd5a9635
jhasudarshan <115224874+jhasudarshan@users.noreply.github.com>
2024-03-25 13:41:14 +0530

    Register Controller Bug Fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,9 +18,9 @@ const registerUser = asyncHandler( async (req,res) => {
 
 
     const  {fullname,email,username,password} = req.body
-    console.log("email: ",email);
-    console.log("fullname: ",fullname);
-    console.log("username: ",username);
+    // console.log("email: ",email);
+    // console.log("fullname: ",fullname);
+    // console.log("username: ",username);
 
     // if(fullname === ""){
     //     throw new ApiError(400,"fullname is required")
@@ -42,13 +42,19 @@ const registerUser = asyncHandler( async (req,res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
+    {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar File is Required")
     }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar)
@@ -59,7 +65,7 @@ const registerUser = asyncHandler( async (req,res) => {
     const user = await User.create({
         fullname,
         avatar:avatar.url,
-        coverImage: coverImage.url,
+        coverImage: coverImage?.url || "",
         email,
         password,
         username: username.toLowerCase(),
